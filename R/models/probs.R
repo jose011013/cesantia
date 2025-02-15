937bcbb065dec4cca6d793cc0b7ba2f21f1557fb
josepiedracgr <144265179+josepiedracgr@users.noreply.github.com>
2025-02-15 13:16:44 -0600

    refactor probs.R

--- R/models/probs.R
+++ R/models/probs.R
@@ -5,76 +5,57 @@ library(readxl)
 
 poblacion <- read_rds("data/processed/poblacion.RDS")
 
-estimar_probs <- function(pob) {
+.estimar_probs <- function(pob) {
+  aj_salidas <- Surv(pob$antiguedad_final, pob$cod_evento)
+  cox_mod <- coxph(aj_salidas ~ sexo, data = pob, id = cedula)
+  cox_fit <- survfit(cox_mod, newdata = tibble(sexo = c("f", "m")))
+  
+  return(list(
+    t = cox_fit$time,
+    pstate = cox_fit$pstate
+  ))
+}
 
-  # estimador de aalen-johansen para las salidas
-  aj_salidas <- Surv(
-    pob$antiguedad_final,
-    pob$cod_evento
+# i = 1: activo, i = 2: cese, i = 3: despido, i = 4: pension, i = 5: renuncia
+.extraer_probs <- function(model, estado) {
+  p <- model$pstate[,,estado]
+  colnames(p) <- c("f", "m")
+  return(
+    as_tibble(p) |>
+      mutate(t = model$t, .before = 1)
   )
-  
-  # modelo de cox de multiples estados
-  cox_mod <- coxph(
-    aj_poblacion ~ sexo,
-    data = pob, 
-    id = cedula
-  )
-  
-  # estimacion de probabilidades por sexo
-  sexo <- c("f", "m")
-  cox_fit <- survfit(
-    cox_mod,
-    newdata = tibble(sexo=sexo)
-  )
-  
-  p <- cox_fit$pstate # probabilidades de salida por causa y sexo
-  
-  p_activo <- p[,,1]
-  colnames(p_activo) <- sexo
-  p_activo <- as_tibble(p_activo)
-  
-  p_cese <- p[,,2]
-  colnames(p_cese) <- sexo
-  p_cese <- as_tibble(p_cese)
-  
-  res <- list(
-    t = cox_fit$time, # tiempos de eventos
-    cese = p_cese, # probabilidades de riesgos en competencia acumuladas de salida por cese (antes del tiempo t)
-    activo = p_activo # probabilidades puntuales (al tiempo t) de seguir activo
-  )
-  
+}
+
+probs <- function(pob) {
+  model <- .estimar_probs(pob)
+  res <- lapply(1:5, \(i) .extraer_probs(model, i))
+  names(res) <- c("activo", "cese", "despido", "pension", "renuncia")
+  res <- append(res, list(t=model$t), after = 0)
   return(res)
 }
 
-tiempos_evento <- function(probs, estado) {
-  
-  tt <- cbind(probs[["t"]], probs[[estado]]) |>
-    as_tibble() |>
-    rename(t=1) |>
-    distinct(f, m, .keep_all = T)
-  
+.identificar_tiempos_evento <- function(probs, estado) {
+  tt <- distinct(probs[[estado]], f, m, .keep_all = T)
   return(tt)
 }
 
-probs_udd <- function(t_evento, umbral) {
-  
+probs_udd <- function(probs, estado, umbral) {
+  p <- .identificar_tiempos_evento(probs, estado)
   tt <- 0:umbral
   probs_f <- approx(t_evento$t, t_evento$f, xout=tt)$y
   probs_m <- approx(t_evento$t, t_evento$m, xout=tt)$y
-  
   return(tibble(t=tt, f=probs_f, m=probs_m))
 }
 
-fit_probs <- poblacion |>
-  estimar_probs()
+fit_probs <- probs(poblacion)
 
 p_cese <- fit_probs |>
-  tiempos(estado="cese") |>
+  tiempos_evento(estado="cese") |>
   probs_udd(umbral=420) |>
   rename(qf=f, qm=m)
 
 p_activo <- fit_probs |>
-  tiempos(estado="activo") |>
+  tiempos_evento(estado="activo") |>
   probs_udd(umbral=420) |>
   rename(pf=f, pm=m)
 
